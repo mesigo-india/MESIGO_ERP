1421c2a54478d1382e28f3c99ea5d35c232f9f5d
mesigo-india <mesigoindia@gmail.com>
2026-07-10 14:43:22 +0530

    fix: make generateCsrfToken reuse the existing token to allow multiple forms on the same page

--- classes/Session.php
+++ classes/Session.php
@@ -125,9 +125,10 @@ class Session
      */
     public static function generateCsrfToken(): string
     {
-        $token = bin2hex(random_bytes(32));
-        $_SESSION['csrf_token'] = $token;
-        return $token;
+        if (empty($_SESSION['csrf_token'])) {
+            $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+        }
+        return $_SESSION['csrf_token'];
     }
     
     /**
